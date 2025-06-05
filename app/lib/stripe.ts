a2d77b46cdebe21d098edebb95932e018bdf8991
Gift Mphato <g.mphato92@gmail.com>
2025-06-05 20:22:42 +0100

    update stripe conf

--- app/lib/stripe.ts
+++ app/lib/stripe.ts
@@ -1,13 +1,13 @@
 import Stripe from 'stripe'
-import { loadStripe } from '@stripe/stripe-js'
+import { loadStripe, Stripe as StripeJS } from '@stripe/stripe-js'
 
 // Server-side Stripe instance
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-05-28.basil',
 })
 
 // Client-side Stripe instance
-let stripePromise: Promise<Stripe | null>
+let stripePromise: Promise<StripeJS | null>
 export const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
